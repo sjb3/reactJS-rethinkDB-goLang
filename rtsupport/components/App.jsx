c1c893a59275fc04b2438c9a7d599975b1c3d27b
Sung J Byun <sungjbyun@D-173-250-158-112.dhcp4.washington.edu>
2017-01-13 20:20:38 -0800

    PropTypes case-sensitive!

--- rtsupport/components/App.jsx
+++ rtsupport/components/App.jsx
@@ -1,32 +1,41 @@
 import React, {Component} from 'react';
-import ChannelSection from './channels/ChannelSection.jsx';
+import ChannelSection from './channels/channelSection.jsx';
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      channels: []
-    };
-  }
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            channels: []
+        };
+    }
+
   addChannel(name){
-    let {channels} = this.state;
-    channels.push({id: channels.length, name});
-    this.setState({channels});
-    //TODO: send to server
+      let {channels} = this.state;
+      channels.push({
+          id: channels.length, name
+      });
+      this.setState({channels});
+      //TODO: Send to Server
   }
+
   setChannel(activeChannel){
-    this.setState({activeChannel});
-    //TODO: Get Channels Messages
+      this.setState({activeChannel});
+      //TODO: Get Channels Messages
   }
   render(){
-    return(
-      <ChannelSection
-        channels={this.state.channels}
-        addChannels={this.addChannels.bind(this)}
-        setChannel={this.setChannel.bind(this)}
-      />
-    )
-  }
+        return(
+          <div className='app'>
+            <div className='nav'>
+               <ChannelSection
+                // channels = {this.state.channels}
+                {...this.state}
+                addChannel = {this.addChannel.bind(this)}
+                setChannel = {this.setChannel.bind(this)}
+             />
+            </div>
+          </div>
+        )
+    }
 }
 
 export default App
--- rtsupport/components/channels/ChannelList.jsx
+++ rtsupport/components/channels/ChannelList.jsx
@@ -1,24 +1,30 @@
 import React, {Component} from 'react';
-import Channel from './Channel.jsx';
+import Channel from './channel.jsx';
 
-class ChannelList extends Component{
-  render(){
-    return(
-      <ul>{
-        this.props.channels.map( chan => {
-          <Channel
-            channel={chan}
-            setChannel={this.props.setChannel}
-          />
-        })
-      }</ul>
-    )
-  }
+class ChannelList extends Component {
+    render (){
+        return(
+            <ul>{
+                this.props.channels.map( chan =>{
+                    return <Channel
+                    channel={chan}
+                    key={chan.id}
+                    // setChannel={this.props.setChannel}
+                    // activeChannel={this.props.activeChannel}
+                    {...this.props}
+                    />
+                })
+            }</ul>
+        )
+    }
+
 }
 
 ChannelList.propTypes = {
-  channels: React.PropTypes.array.isRequired,
-  setChannel: React.PropTypes.func.isRequired
+    channels: React.PropTypes.array.isRequired,
+    setChannel: React.PropTypes.func.isRequired,
+    activeChannel: React.PropTypes.object.isRequired
+
 }
 
 export default ChannelList
