e7fa446a0283e7191cb2f738922eaea00aefd91c
Sung J Byun <sungjbyun@D-69-91-158-126.dhcp4.washington.edu>
2017-01-15 19:25:14 -0800

    something wrong with addmessage: no clue yet

--- rtsupport/components/App.jsx
+++ rtsupport/components/App.jsx
@@ -1,24 +1,62 @@
 import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
 import UserSection from './users/UserSection.jsx';
-// import MessageSection from './messages/MessageSection.jsx';
+import MessageSection from './messages/MessageSection.jsx';
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
             channels: [],
-            users: []
+            users: [],
+            messages: [],
+            activeChannel: {},
+            connected: false
         };
     }
 
+  componentDidMount(){
+    let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+    ws.onmessage = this.message.bind(this);
+    ws.onopen = this.open.bind(this);
+    ws.onclose = this.close.bind(this);
+    }
+  message(e){
+    const event = JSON.parse(e.data);
+    if(event.name === 'channel add'){
+      this.newChannel(event.data);
+    }
+  }
+
+  open(){
+    this.setState({connected: true});
+  }
+
+  close(){
+    this.setState({connected: false});
+  }
+
+  newChannel(channel){
+    let {channels} = this.state;
+    channels.push(channel);
+    this.setState({channels});
+  }
+
   addChannel(name){
       let {channels} = this.state;
-      channels.push({
-          id: channels.length, name
-      });
-      this.setState({channels});
+      // channels.push({
+      //     id: channels.length, name
+      // });
+      // this.setState({channels});
       //TODO: Send to Server
+      let msg = {
+        name: 'channel add',
+        data: {
+          id: channels.length,
+          name
+        }
+      }
+      this.ws.send(JSON.stringify(msg))
   }
 
   setChannel(activeChannel){
@@ -57,7 +95,10 @@ class App extends Component {
               {...this.state}
                 setUserName = {this.setUserName.bind(this)}
               />
-
+              <MessageSection
+              {...this.state}
+                addMessage = {this.addMessage.bind(this)}
+              />
             </div>
           </div>
         )
